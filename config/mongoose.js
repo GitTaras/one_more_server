30fc6fbd8190125891be0026a68ed7b4adac0966
Taras <taras.sholtys@gmail.com>
2020-03-03 11:04:21 +0200

    checking success conection

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,6 +3,13 @@ const path = 'mongodb://localhost/twitter';
 
 mongoose.connect(path, {
   useNewUrlParser: true,
+}, (err) => {
+  if (err) {
+    console.log('can\'t connect to db, fuckout from here');
+    process.exit(1);
+  } else {
+    console.log('DB connection success');
+  }
 });
 mongoose.set('debug', true);
 
